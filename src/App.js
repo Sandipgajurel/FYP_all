0f9e651fd1547e02ee5f1eaeb3ba716d977a64da
sandip <laxmangajurel9191@gmail.com>
2022-03-08 22:24:14 +0545

    add routing

--- src/App.js
+++ src/App.js
@@ -7,14 +7,20 @@ import Footer from './Containers/Footer';
 import LoginForm from './Containers/Login';
 import HomePage from "./Containers/Homepage";
 import Error from "./Containers/Error";
+import Dashboard from "./Containers/Dashboard";
+import ProductAdd from "./Containers/Products/ProductAdd";
+import ProductView from "./Containers/Products/ViewProduct";
 const App = () => {
-  
+
   return (
    <>
    <Routes>
      <Route exact path='/' element={<HomePage />}></Route>
      <Route exact path='/login' element={<LoginForm />}></Route>
      <Route exact path='/signup' element={<SignUpForm />}></Route>
+     <Route exact path='/Dashboard' element={<Dashboard />}></Route>
+     <Route exact path='/ProductAdd' element={<ProductAdd />}></Route>
+     <Route exact path='/ProductView' element={<ProductView />}></Route>
      <Route path='*' element={<Error />} />
    </Routes>
    
--- src/index.js
+++ src/index.js
@@ -4,22 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import SignUpForm from './Containers/Signup';
-import Navbar from './Containers/Navbar';
-import Footer from './Containers/Footer';
-import LoginForm from './Containers/Login';
 ReactDOM.render(
     <BrowserRouter>
     <App />
-    {/* <LoginForm /> */}
-    {/* <SignUpForm /> */}
-    {/* <Navbar /> */}
-    {/* <Footer /> */}
     </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
